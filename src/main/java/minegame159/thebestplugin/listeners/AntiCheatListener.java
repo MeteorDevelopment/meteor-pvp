22eb1c8efbef0eb22df818bd8b8ce9f8b3e5b6ef
MineGame159 <petulko08@gmail.com>
2020-10-28 13:45:48 +0100

    Removed anti cheat

--- src/main/java/minegame159/thebestplugin/listeners/AntiCheatListener.java
+++ src/main/java/minegame159/thebestplugin/listeners/AntiCheatListener.java
@@ -5,7 +5,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
-import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,10 +18,8 @@ import org.bukkit.util.NumberConversions;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AntiCheatListener implements Listener {
+public class AntiCheatListener/* implements Listener */{
     private final Map<Player, Location> lastValidPositions = new HashMap<>();
-    private final Map<Player, Location> lastValidPositionsOnGround = new HashMap<>();
-    private final Map<Player, Integer> inAirTicks = new HashMap<>();
     private final Map<Player, Integer> inactiveTicks = new HashMap<>();
 
     private boolean skipTeleportEvent = false;
@@ -30,8 +27,6 @@ public class AntiCheatListener implements Listener {
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
         lastValidPositions.remove(event.getPlayer());
-        lastValidPositionsOnGround.remove(event.getPlayer());
-        inAirTicks.remove(event.getPlayer());
         inactiveTicks.put(event.getPlayer(), 5);
     }
 
@@ -43,32 +38,24 @@ public class AntiCheatListener implements Listener {
         }
 
         lastValidPositions.remove(event.getPlayer());
-        lastValidPositionsOnGround.remove(event.getPlayer());
-        inAirTicks.remove(event.getPlayer());
         inactiveTicks.put(event.getPlayer(), 5);
     }
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         lastValidPositions.remove(event.getEntity());
-        lastValidPositionsOnGround.remove(event.getEntity());
-        inAirTicks.remove(event.getEntity());
         inactiveTicks.put(event.getEntity(), 5);
     }
 
     @EventHandler
     public void onPlayerGameModeChange(PlayerGameModeChangeEvent event) {
         lastValidPositions.remove(event.getPlayer());
-        lastValidPositionsOnGround.remove(event.getPlayer());
-        inAirTicks.remove(event.getPlayer());
         inactiveTicks.put(event.getPlayer(), 5);
     }
 
     @EventHandler
     public void onPlayerVelocity(PlayerVelocityEvent event) {
         lastValidPositions.remove(event.getPlayer());
-        lastValidPositionsOnGround.remove(event.getPlayer());
-        inAirTicks.remove(event.getPlayer());
         inactiveTicks.put(event.getPlayer(), 10);
     }
 
@@ -97,46 +84,15 @@ public class AntiCheatListener implements Listener {
             double horizontalDistance = NumberConversions.square(pos.getX() - lastPos.getX()) + NumberConversions.square(pos.getZ() - lastPos.getZ());
 
             // Speed check
-            if (horizontalDistance > 1.25) {
+            if (horizontalDistance > 6) {
                 // Moving too fast
                 teleportPos = lastPos;
-                sendDebug(player, "Speed, value %.3f", horizontalDistance);
-            } else {
-                // Flight check
-                double verticalDistance = pos.getY() - lastPos.getY();
-
-                if (!isOnGround(player) && !player.isSwimming()) {
-                    // Flight - In Air check
-                    if (verticalDistance < 0.1 && verticalDistance > -0.1) {
-                        int airTicks = inAirTicks.getOrDefault(player, 0);
-                        airTicks++;
-
-                        if (airTicks > 8) {
-                            // Flying
-                            airTicks = 0;
-                            teleportPos = lastValidPositionsOnGround.getOrDefault(player, lastPos);
-                            sendDebug(player, "Flight - In Air");
-                        }
-
-                        inAirTicks.put(player, airTicks);
-                    } else {
-                        inAirTicks.put(player, 0);
-                    }
-
-                    // Flight - Fast vertical acceleration check
-                    if (verticalDistance > 1) {
-                        teleportPos = lastValidPositionsOnGround.getOrDefault(player, lastPos);
-                        sendDebug(player, "Flight - Fast Acceleration, value %.3f", verticalDistance);
-                    }
-                }
+                sendDebug(player, "Moving too fast, value %.3f", horizontalDistance);
             }
 
             if (teleportPos == null) {
                 // Passed all checks
                 lastValidPositions.put(player, pos);
-                if (isOnGround(player)) {
-                    lastValidPositionsOnGround.put(player, pos);
-                }
             } else {
                 // Didn't pass some checks
                 skipTeleportEvent = true;
@@ -147,15 +103,5 @@ public class AntiCheatListener implements Listener {
 
     private void sendDebug(Player player, String format, Object... args) {
         player.sendMessage(String.format("%s[%sAC%s] %s", ChatColor.GRAY, ChatColor.RED, ChatColor.GRAY, ChatColor.WHITE) + String.format(format, args));
-    }
-
-    private boolean isOnGround(Player player) {
-        Location location = player.getLocation();
-        double w = player.getWidth() / 2;
-
-        if (location.add(w, 0, w).getBlock().getRelative(BlockFace.DOWN).getType().isSolid()) return true;
-        if (location.subtract(w * 2, 0, w * 2).getBlock().getRelative(BlockFace.DOWN).getType().isSolid()) return true;
-        if (location.add(w, 0, w).add(w, 0, -w).getBlock().getRelative(BlockFace.DOWN).getType().isSolid()) return true;
-        return location.subtract(w, 0, -w).add(-w, 0, w).getBlock().getRelative(BlockFace.DOWN).getType().isSolid();
     }
 }
