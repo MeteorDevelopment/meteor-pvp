ae359a86d86ca776593a883844d44a04ce0a504d
MineGame159 <petulko08@gmail.com>
2021-05-02 13:13:50 +0200

    Patched ender chest burrow

--- src/main/java/minegame159/meteorpvp/listeners/AntiCheatListener.java
+++ src/main/java/minegame159/meteorpvp/listeners/AntiCheatListener.java
@@ -6,6 +6,7 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,9 +20,26 @@ public class AntiCheatListener implements Listener {
             if (player.isDead() || player.getGameMode() == GameMode.SPECTATOR) continue;
 
             // Burrow
-            if (isInBlock(player)) {
+            if (isInBlock(player.getLocation())) {
                 Location pos = lastValidBurrowPositions.get(player);
-                if (pos != null) player.teleport(pos);
+                if (pos != null) {
+                    if (isInBlock(pos)) {
+                        // Find new location if previous one is inside block too
+                        Location loc = pos.clone();
+
+                        for (int i = 0; i < 5; i++) {
+                            loc.set(pos.getX(), pos.getY(), pos.getZ());
+                            offset(loc, i);
+
+                            if (isEmptyForPlayer(loc)) {
+                                pos.set(loc.getX(), loc.getY(), loc.getZ());
+                                break;
+                            }
+                        }
+                    }
+
+                    player.teleport(pos);
+                }
             }
             else {
                 lastValidBurrowPositions.put(player, player.getLocation());
@@ -29,7 +47,25 @@ public class AntiCheatListener implements Listener {
         }
     }
 
-    private boolean isInBlock(Player player) {
-        return player.getLocation().getBlock().getType().isOccluding();
+    private boolean isInBlock(Location loc) {
+        Material block = loc.getBlock().getType();
+
+        if (block == Material.SOUL_SAND || block == Material.ENDER_CHEST) return (loc.getY() - (int) loc.getY()) < 0.87;
+
+        return block.isOccluding();
+    }
+
+    private void offset(Location loc, int i) {
+        switch (i) {
+            case 0: loc.add(0, 1, 0);
+            case 1: loc.add(1, 0, 0);
+            case 2: loc.add(-1, 0, 0);
+            case 3: loc.add(0, 0, 1);
+            case 4: loc.add(0, 0, -1);
+        }
+    }
+
+    private boolean isEmptyForPlayer(Location loc) {
+        return loc.getBlock().isEmpty() && loc.getWorld().getBlockAt(loc.getBlockX(), loc.getBlockY() + 1, loc.getBlockZ()).isEmpty();
     }
 }
