a6b062d1c494ad7701329899a672e71114dee7c5
seasnail8169 <seasnail@meteorclient.com>
2021-11-30 21:03:57 +0000

    reverted duels, hopefully fixed

--- src/main/java/meteordevelopment/meteorpvp/duels/Duel.java
+++ src/main/java/meteordevelopment/meteorpvp/duels/Duel.java
@@ -32,28 +32,29 @@ public class Duel implements Listener {
     private final DuelsMode mode;
     private final World world;
     private final Region region;
-    private final int x, z;
 
-    private Player player1, player2, winner;
+    private final Location player1Location;
+    private final Location player2Location;
 
-    private boolean starting, fighting, finishing, finished;
+    private Player player1;
+    private Player player2;
 
-    private int timer;
+    private boolean preparing, wonBeforePrepare;
+    private boolean started, starting;
+    private int startTimer;
 
     public Duel(DuelsMode mode, World world, int x, int z) {
         this.mode = mode;
         this.world = world;
-        this.x = x;
-        this.z = z;
         this.region = new CuboidRegion(BukkitAdapter.adapt(world), BlockVector3.at(x, 0, z), BlockVector3.at(x + SIZE, 119, z + SIZE));
 
-        Bukkit.getPluginManager().registerEvents(this, MeteorPvp.INSTANCE);
+        this.player1Location = new Location(world, x + 15, 5, z + 15);
+        this.player2Location = new Location(world, x + SIZE - 15, 5, z + SIZE - 15);
 
+        Bukkit.getPluginManager().registerEvents(this, MeteorPvp.INSTANCE);
     }
 
     public void start(Player player1, Player player2) {
-        if (finishing || finished) return;
-
         this.player1 = player1;
         this.player2 = player2;
 
@@ -61,66 +62,69 @@ public class Duel implements Listener {
         Duels.INSTANCE.duels.put(player1, this);
         Duels.INSTANCE.duels.put(player2, this);
 
+        preparing = true;
+        wonBeforePrepare = false;
+
         player1.sendMessage(Prefixes.DUELS + "Preparing arena.");
         player2.sendMessage(Prefixes.DUELS + "Preparing arena.");
 
         TaskManager.IMP.async(() -> {
-            if (finishing || finished) return;
-
             try (EditSession editSession = FaweAPI.getEditSessionBuilder(BukkitAdapter.adapt(world)).fastmode(true).build()) {
                 editSession.replaceBlocks(region, new InverseSingleBlockTypeMask(editSession, BlockTypes.BEDROCK), BlockTypes.AIR);
             }
 
-            TaskManager.IMP.sync(() -> {
-                player1.teleport(new Location(world, x + 15, 5, z + 15));
-                player2.teleport(new Location(world, x + SIZE - 15, 5, z + SIZE - 15));
+            preparing = false;
+            if (wonBeforePrepare) {
+                wonBeforePrepare = false;
+                return;
+            }
 
+            TaskManager.IMP.sync(() -> {
+                player1.teleport(player1Location);
+                player2.teleport(player2Location);
+
+                started = true;
                 starting = true;
-                timer = 20 * 5;
+                startTimer = 20 * 5;
 
                 return null;
             });
         });
     }
 
+    public boolean isIn(Location pos) {
+        return region.contains(pos.getBlockX(), pos.getBlockY(), pos.getBlockZ());
+    }
+
+    public boolean isIn(Entity entity) {
+        return isIn(entity.getLocation());
+    }
+
+    public Player getOther(Player player) {
+        if (player1 == player) return player2;
+        return player1;
+    }
+
     @EventHandler
     public void onTick(ServerTickStartEvent event) {
-        if (starting) {
-            if (timer > 0) {
-                switch (timer) {
-                    case 20 * 5 -> setTitle("5");
-                    case 20 * 4 -> setTitle("4");
-                    case 20 * 3 -> setTitle("3");
-                    case 20 * 2 -> setTitle("2");
-                    case 20 -> setTitle("1");
-                }
+        if (!started) return;
 
-                timer--;
-                return;
-            }
+        if (startTimer > 0) {
+            if (startTimer == 20 * 5) setTitle("5");
+            else if (startTimer == 20 * 4) setTitle("4");
+            else if (startTimer == 20 * 3) setTitle("3");
+            else if (startTimer == 20 * 2) setTitle("2");
+            else if (startTimer == 20) setTitle("1");
 
-            timer = 0;
-            starting = false;
-            fighting = true;
+            startTimer--;
+            return;
         }
 
-        if (finishing) {
-            if (timer >= 20 * 10)  {
-                Utils.resetToSpawn(winner);
+        starting = false;
+    }
 
-                mode.makeAvailable(this);
-                Duels.INSTANCE.duels.remove(winner, this);
-
-                finishing = false;
-                finished = true;
-
-                ServerTickStartEvent.getHandlerList().unregister(this);
-
-                return;
-            }
-
-            timer++;
-        }
+    private boolean is(Entity entity) {
+        return player1 == entity || player2 == entity;
     }
 
     @EventHandler
@@ -137,65 +141,50 @@ public class Duel implements Listener {
         }
     }
 
-    private void win(Player winner) {
-        this.winner = winner;
-
-        Duels.INSTANCE.duels.remove(getOther(winner), this);
-        Utils.resetToSpawn(getOther(winner));
-
-        Bukkit.broadcastMessage(String.format("%s %s%s %swon duel against %s%s.", Prefixes.DUELS, ChatColor.GOLD, winner.getName(), ChatColor.GRAY, ChatColor.RED, getOther(winner).getName()));
-
-        if (starting) {
-            player1.resetTitle();
-            player2.resetTitle();
-        }
-
-        PlayerQuitEvent.getHandlerList().unregister(this);
-        PlayerMoveEvent.getHandlerList().unregister(this);
-        PlayerDeathEvent.getHandlerList().unregister(this);
-
-        starting = false;
-        fighting = false;
-        finishing = true;
-        finished = false;
-    }
-
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
-        if (!starting || !is(event.getPlayer()) || Utils.isAdmin(event.getPlayer())) return;
+        if (starting && is(event.getPlayer())) {
+            Location from = event.getFrom();
+            Location to = event.getTo();
 
-        Location from = event.getFrom();
-        Location to = event.getTo();
+            double deltaX = to.getX() - from.getX();
+            double deltaZ = to.getZ() - from.getZ();
 
-        double deltaX = to.getX() - from.getX();
-        double deltaZ = to.getZ() - from.getZ();
-
-        if (deltaX != 0 || deltaZ != 0) {
-            event.getTo().setX(from.getX());
-            event.getTo().setZ(from.getZ());
+            if (deltaX != 0 || deltaZ != 0) {
+                event.getTo().setX(from.getX());
+                event.getTo().setZ(from.getZ());
+            }
         }
+    }
+
+    private void win(Player player) {
+        Player p1 = player1;
+        Player p2 = player2;
+
+        Utils.resetToSpawn(p1);
+        Utils.resetToSpawn(p2);
+
+        if (starting) {
+            p1.resetTitle();
+            p2.resetTitle();
+        }
+
+        if (preparing) wonBeforePrepare = true;
+
+        mode.makeAvailable(this);
+        Duels.INSTANCE.duels.remove(player1, this);
+        Duels.INSTANCE.duels.remove(player2, this);
+
+        Bukkit.broadcastMessage(String.format("%s %s%s %swon duel against %s%s.", Prefixes.DUELS, ChatColor.GOLD, player.getName(), ChatColor.GRAY, ChatColor.RED, getOther(player).getName()));
+
+        player1 = null;
+        player2 = null;
+        started = false;
     }
 
     private void setTitle(String text) {
         Title title = new Title(text, null, 4, 12, 4);
         player1.sendTitle(title);
         player2.sendTitle(title);
-    }
-
-    private boolean is(Entity entity) {
-        return player1 == entity || player2 == entity;
-    }
-
-    public boolean isIn(Location pos) {
-        return region.contains(pos.getBlockX(), pos.getBlockY(), pos.getBlockZ());
-    }
-
-    public boolean isIn(Entity entity) {
-        return isIn(entity.getLocation());
-    }
-
-    public Player getOther(Player player) {
-        if (player1 == player) return player2;
-        return player1;
     }
 }
