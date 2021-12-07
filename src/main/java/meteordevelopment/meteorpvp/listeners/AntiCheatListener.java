3fa5175962ac679037d43baa84962ce1168a0d5f
MineGame159 <petulko08@gmail.com>
2021-12-07 16:02:49 +0100

    AAAAAAAAA

--- src/main/java/meteordevelopment/meteorpvp/listeners/AntiCheatListener.java
+++ src/main/java/meteordevelopment/meteorpvp/listeners/AntiCheatListener.java
@@ -13,6 +13,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class AntiCheatListener implements Listener {
     private final Object2ObjectMap<Player, Location> lastValidBurrowPositions = new Object2ObjectOpenHashMap<>();
@@ -24,6 +25,11 @@ public class AntiCheatListener implements Listener {
                 && event.getEntity().getTicksLived() <= Config.MIN_CRYSTAL_AGE) {
             event.setCancelled(true);
         }
+    }
+
+    @EventHandler
+    private void onPlayerQuit(PlayerQuitEvent event) {
+        lastValidBurrowPositions.remove(event.getPlayer());
     }
 
     @EventHandler
